90250093f2616e57c1415d86194248c825f021f2
Janajeet <janajeetbose@gmail.com>
2022-11-01 00:49:54 +0530

    Frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,84 +1,74 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import Headers from "../components/headers"
+import {
+  useActiveListings,
+  useContract,
+  MediaRenderer,
+} from "@thirdweb-dev/react";
+import { ListingType } from "@thirdweb-dev/sdk";
+import { BanknotesIcon, ClockIcon } from "@heroicons/react/24/outline";
 
-const Home: NextPage = () => {
+const Home = () => {
+  const { contract } = useContract(process.env.NEXT_PUBLIC_MARKETPLACE_CONTRACT, "marketplace");
+
+  const { data: listings, isLoading:loadingListings}=useActiveListings(contract);
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="">
+      <Headers />
+      <main className="max-w-6-xl mx-auto p-5">
+        {loadingListings ? (
+          <p className="text-center animate-pulse text-blue-500">Loading listings...</p>
+        ) : 
+        (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mx-auto">
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+            {listings?.map(listings => (
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+            <div className="flex flex-col card hover:scale-105 transition-all duration-150 ease-out" key={listings.id}>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
+              <div className="flex-1 flex flex-col pb-2 items-center">
 
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+                <MediaRenderer className="w-44" src ={listings.asset.image} /> 
+              </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+                <div className="pt-2 space-y-4">
+                  <div>
+                  <h2 className="text-lg truncate">{listings.asset.name}</h2>
+                  <hr></hr>
+                   <p className="truncate text-sm text-gray-600 mt-2">{listings.asset.description}</p>
+                   </div>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+                   <p>
+                    <span className="font-bold mr-1">
+                      {listings.buyoutCurrencyValuePerToken.displayValue}
+                    </span>{listings.buyoutCurrencyValuePerToken.symbol}
+                   </p>
+
+                   <div 
+                   className={`flex items-center space-x-1 justify-end text-xs border w-fit ml-auto p-2 rounded-lg text-white ${
+                    listings.type === ListingType.Direct
+                     ? "bg-blue-400" 
+                     : "bg-red-500"
+                    } `}>
+                    <p>
+                      {listings.type === ListingType.Direct ?
+                      "Buy Now" : "Auction"}
+                    </p>
+                    {listings.type === ListingType.Direct ?
+                   (<BanknotesIcon className="h-4 "/> )
+                   : 
+                   (<ClockIcon />)
+                  }
+                   </div>
+                </div>
+            </div>
+            ))}
+
+          </div>
+        )
+        }
+
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
   )
 }
